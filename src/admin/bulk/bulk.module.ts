ad35987d170fab1e0e9c7aa386e341ce95bc8d0b
shue <aprosvirov093@gmail.com>
2026-01-23 20:47:13 +0300

    Fix BulkModule: add DatabaseModule and RabbitMQModule imports

--- src/admin/bulk/bulk.module.ts
+++ src/admin/bulk/bulk.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { BulkOperationsController } from "./bulk.controller";
 import { BulkOperationsService } from "./bulk.service";
+import { DatabaseModule } from "../../db/database.module";
+import { RabbitMQModule } from "../../rabbitmq/rabbitmq.module";
 
 @Module({
+  imports: [DatabaseModule, RabbitMQModule],
   controllers: [BulkOperationsController],
   providers: [BulkOperationsService],
   exports: [BulkOperationsService],
